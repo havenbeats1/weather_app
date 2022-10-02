c327816af9d08bea17e818c8f974d21f3f7cc5d8
haven rutter <108587591+havenbeats1@users.noreply.github.com>
2022-10-02 11:56:06 -0400

    weatherapp

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return <div>
+    <div className='flex items-center justify-center my-6'>
+    
+    <p className="text-white text-xl font-extralight">
+        Sunday, 2 October 2022 | Local time: 10:11 AM
+    </p>
+    </div>
+    
+    <div className='flex items-center justify-center my-3'>
+        <p className='text-white text-3xl font-medium'>
+            Derby, CT
+        </p>
+    </div>
+  </div>
+}
+export default TimeAndLocation;
---
+++ src/services/weatherService.js
@@ -0,0 +1,16 @@
+const API_KEY = 'd3fa564c83d11e9a6cfbfb723905c84a';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/'
+
+const getWeatherData = (infoType, searchParams) => {
+    const url = new URL(BASE_URL + '/' + infoType)
+    url.search =new URLSearchParams({...searchParams, appid:API_KEY}
+    )
+
+
+    console.log(url);
+    return fetch(url)
+    .then((res) => res.json())
+    .then((data) => data);
+};
+
+export default getWeatherData;
